adb916a0425ff13ff28439733237cba95f20e071
PeiQi <50662341+MoJiaBings@users.noreply.github.com>
2022-07-31 17:25:49 +0800

    首次提交

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+const program = require('commander');
+
+program
+  // 定义命令和参数
+  .command('create <app-name>')
+  .description('创建一个新项目')
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option('-f, --force', '如果目标目录存在，则覆盖它')
+  .action((name, options) => {
+    require('../lib/create.js')(name, options)
+})
+
+program
+   // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+  
+// 解析用户执行命令传入参数
+program.parse(process.argv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
